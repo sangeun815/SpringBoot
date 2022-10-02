eb0b5779cd1f6541f9f19d26c736b89905654f35
sangeun815 <grace0857@naver.com>
2022-10-02 22:25:32 +0900

    chapter04_JWT생성 및 인증

--- demo/src/main/java/com/example/demo/dto/UserDTO.java
+++ demo/src/main/java/com/example/demo/dto/UserDTO.java
@@ -1,6 +1,5 @@
 package com.example.demo.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class UserDTO {
-
     private String token;
     private String email;
     private String username;
